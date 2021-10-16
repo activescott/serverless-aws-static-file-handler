f6e0d264477dcd7474271c74a812db0e614f7fd6
Scott Willeke <scott@willeke.com>
2021-10-16 14:51:17 -0700

    chore: fix bash-specific stuff in script

--- test-files/scripts/test-http.sh
+++ test-files/scripts/test-http.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env sh
 ## allows curl to some known HTTP endpoint and confirm an expected status code
 URL_PATH=$1
-if [[ -z "$URL_PATH" ]]; then
+if [ -z "$URL_PATH" ]; then
   echo "path not specified"
   return 1
 fi
 EXPECT_CODE=$2
-if [[ -z "$EXPECT_CODE" ]]; then
+if [ -z "$EXPECT_CODE" ]; then
+  echo "EXPECT_CODE not specified; defaulting to 200"
   EXPECT_CODE=200
 fi
 
 HTTP_CODE=$(curl -s -w '%{http_code}' --compressed --output /dev/null "$URL_PATH")
-if [[ "$HTTP_CODE" = "$EXPECT_CODE" ]]; then
+if [ "$HTTP_CODE" = "$EXPECT_CODE" ]; then
   echo "$URL_PATH succeeded (returned expected code $HTTP_CODE)"
+  exit 0
 else
   echo "FAILURE: Expected $URL_PATH to have code $EXPECT_CODE but it was $HTTP_CODE"
-  exit $HTTP_CODE
+  exit 1
 fi
