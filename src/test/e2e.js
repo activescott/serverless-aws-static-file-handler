efc292b2e60757a88828c713a4a83a2988f212fe
Scott Willeke <scott@willeke.com>
2022-02-04 20:39:58 -0800

    chore: detect and ignore failure on node v10. This appears to get all tests working even with serverless v3 on node v10

--- src/test/e2e.js
+++ src/test/e2e.js
@@ -6,12 +6,25 @@ chai.use(chaiAsPromised)
 const expect = chai.expect
 const path = require("path")
 const { spawnSync } = require("child_process")
+const { versions } = require("process")
+
+function nodeVersionMajor() /* : Number */ {
+  return Number(versions.node.split(".")[0])
+}
 
 describe("e2e", function () {
   // npm install & serverless loading a project is pretty slow (apparently damn slow on node8: https://travis-ci.org/activescott/serverless-aws-static-file-handler/jobs/632405805?utm_medium=notification&utm_source=github_status)
   this.timeout(60000)
 
   it("should load plugin", function () {
+    // Serverless Framework v3 does not support Node.js v10. Please upgrade Node.js to the latest LTS version (v12 is a minimum supported version):
+    if (nodeVersionMajor() < 12) {
+      console.warn(
+        "Serverless Framework v3 does not support Node.js v10, skipping test"
+      )
+      this.skip()
+      return
+    }
     // does a simple load of a plugin per https://github.com/activescott/serverless-aws-static-file-handler/issues/32
     const proc = loadServerless("../../test-files/basic-project")
     expect(proc).to.haveOwnProperty("status", 0)
@@ -42,7 +55,8 @@ function loadServerless(projectDir) {
     cwd: cwd,
   })
   if (slsProc.status !== 0) {
-    console.error("proc.stdout", slsProc.stdout.toString())
+    console.error("proc.stdout:", slsProc.stdout.toString())
+    console.error("proc.stderr:", slsProc.stderr.toString())
   }
   return slsProc
 }
